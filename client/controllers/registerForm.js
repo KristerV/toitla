3a5840a56190b47a9956c0f42b3d6619ca9242c9
Mikk Mangus <mikk.mangus@defendec.com>
2014-10-18 19:34:00 +0300

    register form modifications

--- client/controllers/registerForm.js
+++ client/controllers/registerForm.js
@@ -5,6 +5,8 @@ Template.registerForm.events({
 		var form = $('form[name="register"]')
 		var email = form.find('input[name="email"]').val()
 
+
+
 		var user = {
 			username: email,
 			email: email,
@@ -12,9 +14,24 @@ Template.registerForm.events({
 		}
 
 		Accounts.createUser(user, function(e) {
-			if (e) console.log(e)
-
-			Global.closeOverlay()
+			if (e) {
+				console.log(e);
+				//if ()
+			}
+			else {
+				// success
+				Global.closeOverlay()				
+			}
 		})
 	}
 })
+
+Accounts.validateNewUser(function(user) {
+	if (!Global.validateEmail(user.email)) {
+		 throw new Meteor.Error(403, T("email not valid"));
+	}
+	if (!user.password || user.password.length < 8) {
+		 throw new Meteor.Error(403, T("password not long enough"));
+	}
+}) 
+
--- client/lib/Global.js
+++ client/lib/Global.js
@@ -5,6 +5,10 @@ Global = {
 	closeOverlay : function() {
 		Session.set("overlayContent", false)
 		window.location.hash = ''
-	}
+	},
+	validateEmail : function(email) { 
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email);
+	} 
 }
 
