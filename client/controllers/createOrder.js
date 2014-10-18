8cadead39907f793ca4bd598d5977cb41cb72fbe
Janika <janika.liiv@gmail.com>
2014-10-18 22:24:45 +0300

    one row inputs

--- client/controllers/createOrder.js
+++ client/controllers/createOrder.js
@@ -1,3 +1,8 @@
+Template.createOrder.helpers({
+	tomorrow: function(){
+		return moment().add('1 day').format('DD.MM.YYYY')
+	}
+})
 Template.createOrder.events({
 	'submit form[name="createOrder"]': function(e, tmpl) {
 		e.preventDefault()
