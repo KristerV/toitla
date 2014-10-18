8ca16609b90109b90d70382c76fca67b87444197
Mikk Mangus <mikk.mangus@defendec.com>
2014-10-18 20:05:00 +0300

    Chef registration form

--- client/lib/Global.js
+++ client/lib/Global.js
@@ -6,9 +6,13 @@ Global = {
 		Session.set("overlayContent", false)
 		window.location.hash = ''
 	},
-	validateEmail : function(email) { 
-		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return re.test(email);
-	} 
+	addError : function(elem, errorMessage) {
+		var err = $('<span class="error">').text(T(errorMessage))
+			.insertAfter(elem)
+	},
+	removeErrors : function() {
+		$('span.error').remove()
+		$('.error').removeClass('error')
+	}
 }
 
