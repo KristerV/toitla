34ba427a4a120e1a8dfc483e60f522884286de62
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-10-19 02:13:38 +0300

    offer block WIP

--- client/controllers/orders.js
+++ client/controllers/orders.js
@@ -1,5 +1,25 @@
 Template.orders.helpers({
 	orders: function() {
-		return OrderCollection.find({'offers.chefId': Meteor.userId()})
+		return OrderCollection.find()
+	},
+	itemExists: function() {
+		var order = OrderCollection.findOne({'offers.chefId': Meteor.userId()})
+		console.log(order)
+		return order
+	}
+})
+
+Template.orders.events({
+	'submit form[name="offer"]': function(e, tmpl) {
+		e.preventDefault()
+		var form = $(e.currentTarget)
+		var values = Global.getFormValues(form)
+		var orderId = form.attr('id')
+
+		// var order = OrderCollection.findOne({_id: orderId, 'offers.chefId': Meteor.userId()})
+		OrderCollection.update(orderId, {$pull: {'offers.chefId': Meteor.userId()}})
+
+		values['chefId'] = Meteor.userId()
+		OrderCollection.update(orderId, {$push: {offer: values}})
 	}
 })
--- client/lib/Global.js
+++ client/lib/Global.js
@@ -25,13 +25,16 @@ Global = {
 		$('.error').removeClass('error')
 	},
 	getFormValues: function(formName) {
-		var values = $('form[name="'+formName+'"]').serializeArray()
+		if (formName instanceof jQuery)
+			var values = formName.serializeArray()
+		else
+			var values = $('form[name="'+formName+'"]').serializeArray()
 		var data = {}
 		for (var i = 0; i < values.length; i++) {
 			var a = values[i]
 			data[a.name] = a.value
 		};
 		return data
-	}
+	},
 }
 
