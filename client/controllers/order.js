fc21198e96ba68b2962119f129987a54ae9dbaeb
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-10-19 08:52:23 +0300

    order has events

--- client/controllers/order.js
+++ client/controllers/order.js
@@ -30,3 +30,26 @@ Template.order.helpers({
 		return moment(this.timestamp)
 	}
 })
+
+Template.order.events({
+	'submit form[name="offer"]': function(e, tmpl) {
+		e.preventDefault()
+		var form = $(e.currentTarget)
+		var values = Global.getFormValues(form)
+		var orderId = form.attr('id')
+
+		var data = {offers: {}}
+		data.offers[Meteor.userId()] = values
+
+		OrderCollection.update(orderId, {$set: data})
+		console.log(OrderCollection.findOne(orderId))
+	},
+	'submit form[name="chat"]': function(e, tmpl) {
+		e.preventDefault()
+		var form = $(e.currentTarget)
+		var values = Global.getFormValues(form)
+		values['author'] = Meteor.userId()
+		values['timestamp'] = TimeSync.serverTime()
+		OrderCollection.update(form.attr('id'), {$push: {messages: values}})
+	}
+})
--- client/controllers/orders.js
+++ client/controllers/orders.js
@@ -4,25 +4,3 @@ Template.orders.helpers({
 	},
 })
 
-Template.orders.events({
-	'submit form[name="offer"]': function(e, tmpl) {
-		e.preventDefault()
-		var form = $(e.currentTarget)
-		var values = Global.getFormValues(form)
-		var orderId = form.attr('id')
-
-		var data = {offers: {}}
-		data.offers[Meteor.userId()] = values
-
-		OrderCollection.update(orderId, {$set: data})
-		console.log(OrderCollection.findOne(orderId))
-	},
-	'submit form[name="chat"]': function(e, tmpl) {
-		e.preventDefault()
-		var form = $(e.currentTarget)
-		var values = Global.getFormValues(form)
-		values['author'] = Meteor.userId()
-		values['timestamp'] = TimeSync.serverTime()
-		OrderCollection.update(form.attr('id'), {$push: {messages: values}})
-	}
-})
