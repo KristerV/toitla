dd9e295593cd3996be8ef9d3065ab981e935259c
Janika <janika.liiv@gmail.com>
2014-10-25 17:59:21 +0300

    changed the copy of order notification to chef

--- server/Mailer.js
+++ server/Mailer.js
@@ -49,7 +49,7 @@ Meteor.methods({
 			+ T("Your order has been sent to home chefs near you and soon someone will make a delicious offer") + '\n\n'
 			+ T("When this happens we will send you an email.") + '\n\n'
 			+ T("You will see the chefs' offers and can communicate them on your order page at: ") + link + '\n\n'
-			+ '\n\n' + '\n\n'
+			+ '\n\n'
 			+ T("With Kind Regards") + '\n\n'
 			+ "toitla.com"
 			
@@ -74,14 +74,24 @@ Meteor.methods({
 
 		var email = chef.emails[0].address
 		var desc = order.info.description
+		var createdAt = order.info.createdAt
+		var deadline = order.info.date + " " + order.info.time
 		// TODO replace localhost
 		var link = Configuration.site_address + '/chef/' + chef._id
 
-		var subject = T("New order:") + desc
-		var text = T("A new order has arrived, check it out:") + '\n\n'
-			+ desc + '\n\n'
-			+ T("To get more information and see more orders visit:") + '\n'
-			+ link
+		var subject = T('Oven warm hands!') + '' //WTF!? ilma tühja string lisamata ütleb, et subject on objekt mitte string
+		var text = T("Warm up your oven and make flour up your hands!") + '\n'
+			+ createdAt + " " + T("a new order was created:")+ '\n\n'
+			+ desc + '\n\n'		
+			+ T("You have time to cook until") + " " + deadline + '\n'
+			+ T("Make an offer or specify the order:") + link + '\n'
+			+ T("Using this link you can always see your offers.") + '\n'
+			+ T("Please answer the client as soon as possible.") + '\n'
+			+ T("If you need help, please call 58 49 43 40 or email us appi@toitla.com") + '\n'
+			+ T("All feedback rocks are world!") + '\n'
+			+ '\n\n'
+			+ T("With Kind Regards") + '\n'
+			+ T("toitla.com team ;)")
 
 		Mailer.send(email, Configuration.email, subject, text)
 	},
