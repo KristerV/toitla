da013d9f08f7212ea1ab04f6bc3230093479cdb7
Mikk Mangus <mikk.mangus@defendec.com>
2014-11-30 13:54:59 +0200

    Half way profile map

--- client/controllers/index.js
+++ client/controllers/index.js
@@ -24,15 +24,19 @@ Template.index.helpers({
 
 Template.index.rendered = function() {
 
+
+
+	document.title = T('Homecooks')
+	$('head').append('<meta name="viewport" content="width=device-width, initial-scale=1">')
+		
+
+	/*
 	var mapOptions = {
       center: { lat: -34.397, lng: 150.644},
       zoom: 8
     };
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-
-	document.title = T('Homecooks')
-	$('head').append('<meta name="viewport" content="width=device-width, initial-scale=1">')
+    */
 
 	if ('undefined' == typeof Configuration) {
 		alert('Hey, developer! \n\n Yes you!\nPlease provide a /lib/configuration.js \n(there is an example also).')
--- client/controllers/overlay/chefProfile.js
+++ client/controllers/overlay/chefProfile.js
@@ -18,11 +18,135 @@ Template.chefProfile.helpers({
 	}
 })
 
+
+var map = false
+var geocoder = false
+var marker = false
+
+var locationByAddress = function(address) {
+	console.log('loc')
+	geocoder.geocode({'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			map.setCenter(results[0].geometry.location);
+			if (marker) {
+				marker.setMap(null)
+			}
+			marker = new google.maps.Marker({
+				map: map,
+				position: results[0].geometry.location
+			});
+		} else {
+			alert("Geocode was not successful for the following reason: " + status);
+		}
+	});
+}
+var updateMap = function() {
+	// if the profile is not present, do not show the map
+	if (0 == $('form[name=chefProfile]').length) {
+		$('#profile-map').css('height', '0')
+		return
+	}
+	$('#profile-map').css('height', '300')
+
+	var values = Global.getFormValues('chefProfile')
+
+	// if city is not set, do not show the map (nor make a query about the location)
+	if (!values.city) {
+		$('#profile-map').css('height', '0')
+		return
+	}
+
+	// find the location by city&street
+    var address = values.city;
+    if (values.street) {
+    	var street = values.street;
+    	if (values.house) {
+    		street += ' ' + values.house;
+    	}
+    	address = street + ', ' + address;
+    }
+    locationByAddress(address);
+
+    /*
+	$.get('http://nominatim.openstreetmap.org/search?format=json&limit=1&city=' +
+		values.city + '&street=' + values.street + '&country=ee', function(data)
+	{
+		var place = data[0]
+
+		if (!place){
+			$('#chefMap').css('height', '0')
+			return
+		}
+
+		if (map)
+			map.destroy()
+			
+		map = new OpenLayers.Map('chefMap', {projection:'EPSG:3857'});
+
+	    var osm = new OpenLayers.Layer.OSM(); // openstreetmap
+	    var toMercator = OpenLayers.Projection.transforms['EPSG:4326']['EPSG:3857'];
+
+	    var feature = new OpenLayers.Feature.Vector(
+	        toMercator(new OpenLayers.Geometry.Point(place.lon, place.lat)),
+	        {loc : place.display_name},
+	        {
+	            fillColor : '#008040',
+	            fillOpacity : 0.8,                    
+	            strokeColor : '#ee9900',
+	            strokeOpacity : 1,
+	            strokeWidth : 1,
+	            pointRadius : 8
+	        }
+        )
+	        
+	    // vectorlayer for programmatic graphics
+	    var vector = new OpenLayers.Layer.Vector('Points')
+	    vector.addFeatures([feature])
+	    map.addLayers([osm, vector])
+	    // set center position and zoom
+	    map.setCenter(new OpenLayers.LonLat(place.lon, place.lat).transform('EPSG:4326', 'EPSG:3857'), 12)
+	})
+	*/
+}
+
+Template.chefProfile.rendered = function() {
+
+
+	setTimeout(function() {
+
+		new google.maps.LatLng(37.7699298, -122.4469157);
+
+		var mapOptions = {
+	      center: { lat: -34.397, lng: 150.644},
+	      zoom: 8
+	    };
+	    map = new google.maps.Map(document.getElementById('profile-map'), mapOptions);
+
+	    geocoder = new google.maps.Geocoder();
+		
+	}, 2000)
+	// update map, when dom is ready,
+	// and wait a bit before you do (let meteor load content)..
+	//$(function() {setTimeout(updateMap, 1000)})	
+}
+
 Template.chefProfile.events({
+	'blur input[name="street"]': function(e, tmpl) {
+		updateMap()
+	},
 	'submit form[name="chefProfile"]': function(e, tmpl) {
 		e.preventDefault()
 		var values = Global.getFormValues('chefProfile')
 		Meteor.users.update(Meteor.userId(), {$set: {profile: values}})
 		Global.closeOverlay()
+
+		$.get('http://nominatim.openstreetmap.org/search?format=json&limit=1&city=' +
+			values.city + '&street=' + values.street + '&country=ee', function(data)
+		{
+			var place = data[0];
+			if (place) {
+				Meteor.users.update(Meteor.userId(), {$set: {'profile.location': place}})
+			}
+		})
 	}
 })
