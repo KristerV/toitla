b08915f1e6155893dcb8e1e8d1edb1368aa425d7
Janika <janika.liiv@gmail.com>
2014-11-30 17:46:46 +0200

    confirm oreder map has range circle

--- client/controllers/confirmOrder.js
+++ client/controllers/confirmOrder.js
@@ -1,5 +1,7 @@
 var autocomplete = null;
 var map = null;
+var rangeCircle = null;
+var marker = null;
 var tallinn = new google.maps.LatLng(59.437222, 24.745278);
 
 var setCurrentLocation = function() {
@@ -13,23 +15,19 @@ var setCurrentLocation = function() {
 var setMarker = function(place) {
   map.setCenter(place.geometry.location)
   map.setZoom(14)
-  marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  })
+  marker.setPosition(place.geometry.location)
+  marker.setVisible(true)
+  drawCircle(1000)
+}
+
+//circle radius on metres
+var drawCircle = function(circleRadius) {
+	rangeCircle.setCenter(marker.getPosition())
+	rangeCircle.setRadius(circleRadius)
+	rangeCircle.setVisible(true);
 }
 
 Template.confirmOrder.rendered = function() {
-	$('.order-range').jRange({
-	    from: 1,
-	    to: 5,
-	    step: 1,
-	    scale: [1, 3, 5, 10, 25],
-	    format: '%s',
-	    showLabels: true,
-	    isRange: true,
-	});
-
  	setCurrentLocation();
   var defaultOptions = {
      center: tallinn,
@@ -37,6 +35,17 @@ Template.confirmOrder.rendered = function() {
    };
   map = new google.maps.Map(document.getElementById('confirmation-map'), defaultOptions);
   autocomplete = new google.maps.places.Autocomplete((document.getElementById('map-autocomplete')),{ types: ['geocode'] });
+  marker = new google.maps.Marker({ map: map, visible: false })
+  var circleOptions = {
+	  strokeColor: '#30947f',
+	  strokeOpacity: 0.2,
+	  strokeWeight: 2,
+	  fillColor: '#30947f',
+	  fillOpacity: 0.2,
+	  visible: false,
+	  map: map
+	}
+  rangeCircle = new google.maps.Circle(circleOptions);
   google.maps.event.addListener(autocomplete, 'place_changed', function(param) {
     setMarker(autocomplete.getPlace());
   });	
@@ -52,5 +61,13 @@ Template.confirmOrder.events({
             geolocation));
       });
     }
+  },
+  'submit form': function(e) {
+  	e.preventDefault();
+  },
+  'change input[type="range"]' : function(e) {
+  	e.preventDefault()
+		var slider = $(e.currentTarget)
+		drawCircle(parseInt(slider.val())*1000)
   }
 })
