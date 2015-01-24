89d193dc7cba09acad39d58637f52899b4ef1e99
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-01-24 18:10:47 +0200

    logging start

--- packages/toitla:common/toitla:common.js
+++ packages/toitla:common/toitla:common.js
@@ -5,5 +5,16 @@ Common = {
 		// SafeString converts raw HTML tags to actual tags.
 		// The replace makes newlines work
 		return new Spacebars.SafeString(text.replace(/(\r\n|\n|\r)/gm, '<br>'))
+	},
+	/*
+	* Replaces %s variables in arg1 with arg2, arg3, ...
+	* stringf(arg1, arg2, arg3, ...)
+	*/
+	stringf: function() {
+		// var regex = /%(s|d)/g
+		// var string = ["üks","kaks","kolm"].pop()
+		// console.log(string)
+
+		// string.replace(regex, function)
 	}
 }
