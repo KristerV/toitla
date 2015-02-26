fc9397d48a69ef88fd66ebbc8910abee3254e503
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-02-26 18:39:43 +0200

    clear away for toitla 3

---
+++ private/toitla2/lib/TmailTemplates.js
@@ -0,0 +1,22 @@
+TmailTemplates = {
+	'test1': {
+		en: {
+			subject: "Test email :)",
+			body: "Oh no, you shouldn't have gotten this email. Please reply to this message and let us know about it!",
+		},
+		et: {
+			subject: "Test email :)",
+			body: "Oih, see kiri ei oleks pidanud sulle tulema, kas palun annaksid Toitla meeskonnale sellest veast teada? Olen vaid väike robot ja ükski inimene ei tea, et ma siin olen.",
+		},
+	},
+	'test2': {
+		en: {
+			subject: "Testing email :) 2",
+			body: "Oh no, you shouldn't have gotten this email. Please reply to this message and let us know about it!",
+		},
+		et: {
+			subject: "Testime email :) 2",
+			body: "Oih, see kiri ei oleks pidanud sulle tulema, kas palun annaksid Toitla meeskonnale sellest veast teada? Olen vaid väike robot ja ükski inimene ei tea, et ma siin olen.",
+		},
+	},
+}
---
+++ private/toitla2/lib/Translations.js
@@ -0,0 +1,18 @@
+Translations = {
+	'confirm_order': {
+		en: 'Confirm order',
+		et: 'Kinnita tellimus'
+	},
+	'place_order': {
+		en: 'Place order',
+		et: 'Telli'
+	},
+	'site_title': {
+		en: 'Toitla - home cooks',
+		et: 'Toitla - kodukokkade kodu',
+	},
+	'you': {
+		en: 'hey %s ho!',
+		et: 'hei %s hou!',
+	},
+}
