db6b5448d0edead8f5fd37e45f1f920f58ded406
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-03-05 19:12:27 +0200

    image uploading

--- client/view/module/post/post.js
+++ client/view/module/post/post.js
@@ -1,21 +1,22 @@
+Template.postNew.helpers({
+	image: function() {
+		var img = Images.findOne(Session.get('uploaded-image'))
+		if (!img)
+			return false
+		return img
+	},
+})
+
 Template.postNew.events({
 	'change #fileselect': function(e, tmpl) {
-		var file = e.target.files[0]
-		console.log(file)
-		var reader  = new FileReader();
-
-		reader.onloadend = function () {
-			// Session.set('photo', reader.result)
-			$('label.full-photo')
-				.css('background-image', 'url('+reader.result+')')
-				.css('color', 'rgba(0,0,0,0)') // hide text
-		}
-
-		if (file) {
-			reader.readAsDataURL(file);
-		}
+		FS.Utility.eachFile(e, function(file) {
+			Images.insert(file, function (err, fileObj) {
+				console.log(err)
+				Session.set('uploaded-image', fileObj._id)
+			});
+		});
 	},
 	'click button.submit': function(e, tmpl) {
-		console.log($('label.full-photo'))
 	}
 })
+
---
+++ server/allow.js
@@ -0,0 +1,11 @@
+Images.allow({
+	insert: function(userId, doc) {
+		return true
+	},
+	update: function(userId, document, fieldNames, modifier) {
+		return true
+	},
+	download: function() {
+		return true
+	}
+})
