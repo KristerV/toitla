1bb1645cf5995dc76ada6102d73514d90f0bcf44
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-03-06 20:51:36 +0200

    fix splitting null

--- client/view/Images.js
+++ client/view/Images.js
@@ -4,9 +4,13 @@ Images = {
 	},
 	getUrl: function(id) {
 		var imgObj = this.getOne(id)
-		url = imgObj ? imgObj.url() : null
+		if (!imgObj)
+			return null
+
+		url = imgObj.url()
 
 		// Remove token, it for some reason creates confusion
+		// Token format: image/link.png?token=xxxx
 		var cleanUrl = url.split('?')[0]
 		return cleanUrl
 	}
