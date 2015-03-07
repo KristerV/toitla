d955f2cb127201def450c4c366cf19cdf054a960
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-03-07 19:44:15 +0200

    minus hasstored

--- client/view/module/post/post.js
+++ client/view/module/post/post.js
@@ -1,7 +1,7 @@
 Template.post.helpers({
 	image: function() {
 		var image = Images.getOne(this.imageId)
-		if (image && image.isUploaded() && image.hasStored())
+		if (image && image.isUploaded())
 			return "background-image:url("+image.url({store: '500'})+")"
 		else
 			return "display:none"
