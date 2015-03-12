937bff771a8e012707d7d16741a13a6bc8ccec5b
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-03-12 20:44:14 +0200

    add missing files

---
+++ packages/toitla:account/facebook/facebook.js
@@ -0,0 +1,27 @@
+Facebook = {
+	share: function(postId) {
+		var post = Posts.getOne(postId)
+		var imageUrl = Images.getUrlByPost(postId, true)
+		var description = Global.shortenString(post.description, 999)
+		var link = Configuration.url() + 'post/' + postId
+		FB.ui({
+			method: 'feed',
+			link: link,
+			caption: T('facebook_caption'),
+			picture: imageUrl,
+			description: description
+		}, function(response){
+			console.log("Sharing complete")
+			console.log(response)
+		})
+	},
+	login: function() {
+		Meteor.loginWithFacebook({}, function(err){
+			if (err) {
+				throw new Meteor.Error(err);
+			} else {
+				Panel.left('userProfileEdit')
+			}
+		})
+	}
+}
