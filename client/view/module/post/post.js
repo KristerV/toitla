71f30487615cfa82292e5e5b39770cc9446501b1
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-03-16 12:57:48 +0200

    better desktop view

--- client/view/module/post/post.js
+++ client/view/module/post/post.js
@@ -11,11 +11,6 @@ Template.post.helpers({
 	author: function() {
 		return User.getProfile(this.author)
 	},
-	thumbSize: function() {
-		var size = Session.get('menubar-width')
-		size = parseInt(size) * 0.6
-		return size + 'px'
-	},
 	like: function() {
 		if (!this.likes)
 			return false
