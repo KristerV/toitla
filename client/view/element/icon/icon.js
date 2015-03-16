783155344fb4af7d7b1437c67d4874445a68497d
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-03-16 15:15:44 +0200

    transparent like not clickable

--- client/view/element/icon/icon.js
+++ client/view/element/icon/icon.js
@@ -1,12 +1,18 @@
 Template.icon.events({
 	'click .btn-like': function(e) {
-		e.stopPropagation()
-		var postId = $(e.delegateTarget).parents('.post').attr('id')
-		Posts.like(postId, Meteor.userId())
+		// If the main feed is not transparent
+		if ($(e.target).parents('.content-bar.transparent').length == 0) {
+			e.stopPropagation()
+			var postId = $(e.delegateTarget).parents('.post').attr('id')
+			Posts.like(postId, Meteor.userId())
+		}
 	},
 	'click .btn-share': function(e) {
-		e.stopPropagation()
-		var postId = $(e.delegateTarget).parents('.post').attr('id')
-		Facebook.share(postId)
+		// If the main feed is not transparent
+		if ($(e.target).parents('.content-bar.transparent').length == 0) {
+			e.stopPropagation()
+			var postId = $(e.delegateTarget).parents('.post').attr('id')
+			Facebook.share(postId)
+		}
 	},
 })
