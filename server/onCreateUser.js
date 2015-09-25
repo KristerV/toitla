eddc542d2c88234c7b3ffcdb43071072511b9f74
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-09-25 18:36:46 +0300

    fix onCreateUser

--- server/onCreateUser.js
+++
@@ -1,5 +0,0 @@
-Accounts.onCreateUser(function(options, user) {
-  var emailHTML = "<p> -------------- Uus kasutaja -------------- </p><p>Email: " + user.emails[0].address + "</p>"
-  Email.send('teavitus@toitla.com', 'conv.1nho427pw5qh36@fleep.io', 'Teavitus tellimusest', emailHTML)
-  return user
-})
