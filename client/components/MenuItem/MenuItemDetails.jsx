f924a39738de9154615c7a6cfc38830fa8f0d28f
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-11-04 12:33:11 +0200

    hide menuitem specifications from inorder

--- client/components/MenuItem/MenuItemDetails.jsx
+++ client/components/MenuItem/MenuItemDetails.jsx
@@ -27,95 +27,104 @@ MenuItemDetails = React.createClass({
         var menuitem = this.props.menuitem
         var errors = menuitem.formErrors || {}
         var editMode = Roles.userIsInRole(Meteor.userId(), 'manager') || (!menuitem.inorder && !menuitem.published)
-        var content
-        if (menuitem) {
-            var user = Meteor.users.findOne(menuitem.chefId) || {profile: {}}
-            content = <div className="padding">
-                <p>{menuitem.chefName || user.profile.name}</p>
-                <TextInput
-                    editMode={editMode}
-                    label="Toidu nimetus"
-                    value={menuitem.title}
-                    name="title"
-                    onBlur={this.updateText}
-                    errorMsg={errors.title}/>
-                <TextInput
-                    editMode={editMode}
-                    label="Koostisosad"
-                    value={menuitem.ingredients}
-                    rows="1"
-                    name="ingredients"
-                    onBlur={this.updateText}
-                    errorMsg={errors.ingredients}/>
 
-                {Settings.menuitemTags.map(function(tag, i){
-                    return <Tag key={i}
-                        label={tag.label}
-                        editMode={editMode}
-                        active={_.contains(menuitem.tags, tag.name)}
-                        name={tag.name}
-                        color={tag.color}
-                        onClick={this.switchTag}/>
-                }.bind(this))}
-
-                <div className="w100">
-                    <div style={{width: '50%'}} className="inline vtop paddingr box">
-                        <DropDown menuItems={[
-                            {value: null, text: 'tüüp'},
-                            {value: 'main', text: 'soolane'},
-                            {value: 'dessert', text: 'magus'},
-                            ]}
-                            name="foodType"
-                            editMode={editMode}
-                            autoWidth={false}
-                            selectedIndex={Settings.getIndexOfSetting('foodTypes', menuitem.foodType, true)}
-                            onChange={this.changeDropdown}
-                            errorMsg={errors.foodType}/>
-                    </div>
-                    <div style={{width: '50%'}} className="inline vtop paddingl box">
-                        <DropDown menuItems={[
-                            {value: null, text: 'Hinnaklass'},
-                            {value: 'class1', text: 'Tükihind '+Settings.priceClasses.class1},
-                            {value: 'class2', text: 'Tükihind '+Settings.priceClasses.class2},
-                            {value: 'class3', text: 'Tükihind '+Settings.priceClasses.class3},
-                            ]}
-                            name="priceClass"
-                            editMode={editMode}
-                            autoWidth={false}
-                            selectedIndex={Settings.getIndexOfSetting('priceClasses', menuitem.priceClass, true)}
-                            onChange={this.changeDropdown}
-                            errorMsg={errors.priceClass}/>
-                    </div>
+        // Placeholder
+        if (!menuitem) {
+            return <section className="padding details">
+                <div className="placeholder padding">
+                    <p>Eesnimi Perenimi</p>
+                    <p>Ilus toidu pealkiri</p>
+                    <p>Pikk ja detailne kirjeldus koostisosadest</p>
+                    <Tag label="lihavaba" active={false} color="#64DD17"/>
+                    <Tag label="vegan" active={false} color="#00C853"/>
+                    <Tag label="toor" active={false} color="#00BFA5"/>
+                    <Tag label="mahe" active={false} color="#8D6E63"/>
+                    <Tag label="gluteenivaba" active={false} color="#0091EA"/>
+                    <Tag label="laktoosivaba" active={false} color="#2962FF"/>
                 </div>
-                <TextInput
-                    editMode={editMode}
-                    label="Kaal (g)"
-                    value={menuitem.weight}
-                    name="weight"
-                    onBlur={this.updateText}
-                    errorMsg={errors.weight}
-                    />
-            </div>
-        } else {
-            content = <div className="placeholder padding">
-                <p>Eesnimi Perenimi</p>
-                <p>Ilus toidu pealkiri</p>
-                <p>Pikk ja detailne kirjeldus koostisosadest</p>
-                <Tag label="lihavaba" active={false} color="#64DD17"/>
-                <Tag label="vegan" active={false} color="#00C853"/>
-                <Tag label="toor" active={false} color="#00BFA5"/>
-                <Tag label="mahe" active={false} color="#8D6E63"/>
-                <Tag label="gluteenivaba" active={false} color="#0091EA"/>
-                <Tag label="laktoosivaba" active={false} color="#2962FF"/>
-            </div>
+            </section>
         }
 
+        var user = Meteor.users.findOne(menuitem.chefId) || {profile: {}}
 
         // Render
         return(
             <section className="padding details">
-                {content}
+                <div className="padding">
+                    <p>{menuitem.chefName || user.profile.name}</p>
+                    <TextInput
+                        editMode={editMode}
+                        label="Toidu nimetus"
+                        value={menuitem.title}
+                        name="title"
+                        onBlur={this.updateText}
+                        errorMsg={errors.title}/>
+                    <TextInput
+                        editMode={editMode}
+                        label="Koostisosad"
+                        value={menuitem.ingredients}
+                        rows="1"
+                        name="ingredients"
+                        onBlur={this.updateText}
+                        errorMsg={errors.ingredients}/>
+
+                    {Settings.menuitemTags.map(function(tag, i){
+                        return <Tag key={i}
+                            label={tag.label}
+                            editMode={editMode}
+                            active={_.contains(menuitem.tags, tag.name)}
+                            name={tag.name}
+                            color={tag.color}
+                            onClick={this.switchTag}/>
+                    }.bind(this))}
+                </div>
+                {getSpecifications()}
             </section>
         )
+    },
+
+    getSpecifications() {
+        if (this.props.menuitem.inorder) {
+            return <div></div>
+        }
+        return <div>
+            <div className="w100">
+                <div style={{width: '50%'}} className="inline vtop paddingr box">
+                    <DropDown menuItems={[
+                        {value: null, text: 'tüüp'},
+                        {value: 'main', text: 'soolane'},
+                        {value: 'dessert', text: 'magus'},
+                        ]}
+                        name="foodType"
+                        editMode={editMode}
+                        autoWidth={false}
+                        selectedIndex={Settings.getIndexOfSetting('foodTypes', menuitem.foodType, true)}
+                        onChange={this.changeDropdown}
+                        errorMsg={errors.foodType}/>
+                </div>
+                <div style={{width: '50%'}} className="inline vtop paddingl box">
+                    <DropDown menuItems={[
+                        {value: null, text: 'Hinnaklass'},
+                        {value: 'class1', text: 'Tükihind '+Settings.priceClasses.class1},
+                        {value: 'class2', text: 'Tükihind '+Settings.priceClasses.class2},
+                        {value: 'class3', text: 'Tükihind '+Settings.priceClasses.class3},
+                        ]}
+                        name="priceClass"
+                        editMode={editMode}
+                        autoWidth={false}
+                        selectedIndex={Settings.getIndexOfSetting('priceClasses', menuitem.priceClass, true)}
+                        onChange={this.changeDropdown}
+                        errorMsg={errors.priceClass}/>
+                </div>
+            </div>
+            <TextInput
+                editMode={editMode}
+                label="Kaal (g)"
+                value={menuitem.weight}
+                name="weight"
+                onBlur={this.updateText}
+                errorMsg={errors.weight}
+            />
+        </div>
     }
 })
