d2a258cc2f369729ef9f502825b0eaaaf3f07516
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-11-04 13:12:03 +0200

    menuitemRefresh generates non-repeating endless supply

--- server/refreshMenuItemsInOrder.js
+++ server/refreshMenuItemsInOrder.js
@@ -2,6 +2,8 @@ RefreshMenuItemsInOrder = function(orderId){
     check(orderId, String);
     var order = Orders.findOne(orderId)
     var menuItemsInOrder = MenuItemsInOrder.find({orderId: orderId, rejected: {$ne: true}}).fetch()
+    var rejectedMenuitems = MenuItemsInOrder.find({orderId: orderId, rejected: true}).fetch()
+    var rejectedTemplateIds = _.pluck(rejectedMenuitems, 'templateId')
     var peopleCount = parseInt(order.details.peopleCount)
     var itemsNeededTotal = peopleCount / 10
     var newItemsRequired = itemsNeededTotal - menuItemsInOrder.length
@@ -11,7 +13,18 @@ RefreshMenuItemsInOrder = function(orderId){
         RefreshMenuItemsInOrder(orderId)
     } else {
         for (var i = 0; i < newItemsRequired; i++) {
-            var newItem = MenuItemTemplates.find({rand: {$gte: Math.random()}}, {sort: {rand: 1}, limit: 1}).fetch()[0]
+            var newItem = MenuItemTemplates.find(
+                {
+                    _id: {$nin: rejectedTemplateIds},
+                    rand: {$gte: Math.random()}
+                },
+                {sort: {rand: 1}, limit: 1}).fetch()[0]
+            if (!newItem)
+                var newItem = MenuItemTemplates.find(
+                    {
+                        rand: {$gte: Math.random()}
+                    }, {sort: {rand: 1}, limit: 1}).fetch()[0]
+            newItem.templateId = newItem._id
             delete newItem._id
             newItem.orderId = orderId
             newItem.inorder = true
