a0c67bb19ee724abb6fbf06c87ab651e8211eab2
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-11-17 19:00:05 +0200

    new order flow logic

--- client/components/Order/NewOrder.jsx
+++ client/components/Order/NewOrder.jsx
@@ -5,32 +5,48 @@ NewOrder = React.createClass({
             return(<Loader/>)
         var order = this.props.order
         order.errors = order.errors || {}
+        order.flow = order.flow || []
+
+        var forms = []
+        for (var i = 0; i < order.flow.length; i++) {
+            var form
+            switch (order.flow[i]) {
+                case "NewOrderSectionContacts":
+                    form = <NewOrderSectionContacts key={i} order={order} flowIndex={0} primaryFlowNext="event_form" secondaryFlowNext="contact_form_finish"/>
+                    break
+                case "NewOrderSectionContactsFinish":
+                    form = <NewOrderSectionContactsFinish key={i} order={order} flowIndex={1} primaryFlowNext="thanks_form"/>
+                    break
+                case "NewOrderSectionEvent":
+                    form = <NewOrderSectionEvent key={i} order={order} flowIndex={1} primaryFlowNext="allergies_form" secondaryFlowNext="event_form_finish"/>
+                    break
+                case "NewOrderSectionEventFinish":
+                    form = <NewOrderSectionEventFinish key={i} order={order} flowIndex={2} primaryFlowNext="thanks_form"/>
+                    break
+                case "NewOrderSectionMenu":
+                    form = <NewOrderSectionMenu key={i} order={order} flowIndex={2} primaryFlowNext="allergies_form" secondaryFlowNext="menu_form_finish"/>
+                    break
+                case "NewOrderSectionMenuFinish":
+                    form = <NewOrderSectionMenuFinish key={i} order={order} flowIndex={3} primaryFlowNext="thanks_form"/>
+                    break
+                case "NewOrderSectionAllergies":
+                    form = <NewOrderSectionAllergies key={i} order={order} flowIndex={3} primaryFlowNext="extra_form"/>
+                    break
+                case "NewOrderSectionExtra":
+                    form = <NewOrderSectionExtra key={i} order={order} flowIndex={4} primaryFlowNext="thanks_form"/>
+                    break
+                case "NewOrderSectionThanks":
+                    form = <NewOrderSectionThanks key={i} order={order} flowIndex={5} primaryFlowNext=""/>
+                    break
+                default:
+                    throw new Meteor.Error("NewOrder form does not exist: " + order.flow[i])
+            }
+            forms.push(form)
+        }
+
         return(<div className="h100">
-
-            {/*Status*/}
             {/*<StatusForm order={order}/>*/}
-
-            {/*Contacts*/}
-            <NewOrderSectionContacts order={order} flowIndex={0} primaryFlowNext="event_form" secondaryFlowNext="contact_form_finish"/>
-            <NewOrderSectionContactsFinish order={order} flowIndex={1} primaryFlowNext="thanks_form"/>
-
-            {/*Event details*/}
-            <NewOrderSectionEvent order={order} flowIndex={1} primaryFlowNext="allergies_form" secondaryFlowNext="event_form_finish"/>
-            <NewOrderSectionEventFinish order={order} flowIndex={2} primaryFlowNext="thanks_form"/>
-
-            {/*Menu*/}
-            <NewOrderSectionMenu order={order} flowIndex={2} primaryFlowNext="allergies_form" secondaryFlowNext="menu_form_finish"/>
-            <NewOrderSectionMenuFinish order={order} flowIndex={3} primaryFlowNext="thanks_form"/>
-
-            {/*Allergies*/}
-            <NewOrderSectionAllergies order={order} flowIndex={3} primaryFlowNext="extra_form"/>
-
-            {/*Extra info*/}
-            <NewOrderSectionExtra order={order} flowIndex={4} primaryFlowNext="thanks_form"/>
-
-            {/*Thanks*/}
-            <NewOrderSectionThanks order={order} flowIndex={5} primaryFlowNext=""/>
-
+            {forms}
         </div>)
     }
 })
