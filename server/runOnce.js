ca65e8207992750c04e4a97260dd2c8d1ec1f4e3
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-11-27 23:29:10 +0200

    disable tag fix runOnce

--- server/runOnce.js
+++ server/runOnce.js
@@ -1,14 +1,15 @@
-Meteor.startup(function(){
-    var items = MenuItemTemplates.find()
-    items.forEach(function(item){
-        if (item.tags && item.tags.length > 0 && !_.isObject(item.tags[0])) {
-            var newTags = []
-            var oldTags = item.tags
-            for (var i = 0; i < oldTags.length; i++) {
-                var newTag = Settings.findByKey('menuitemTags', 'name', oldTags[i])
-                newTags.push(newTag)
-            }
-            MenuItemTemplates.update(item._id, {$set: {tags: newTags}})
-        }
-    })
-});
+// fix tags from arrays to objects
+// Meteor.startup(function(){
+//     var items = MenuItemTemplates.find()
+//     items.forEach(function(item){
+//         if (item.tags && item.tags.length > 0 && !_.isObject(item.tags[0])) {
+//             var newTags = []
+//             var oldTags = item.tags
+//             for (var i = 0; i < oldTags.length; i++) {
+//                 var newTag = Settings.findByKey('menuitemTags', 'name', oldTags[i])
+//                 newTags.push(newTag)
+//             }
+//             MenuItemTemplates.update(item._id, {$set: {tags: newTags}})
+//         }
+//     })
+// });
