5ed48f3f9a77e1582322479784b59773e1b3f207
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-12-01 13:50:56 +0200

    restructure orders

---
+++ client/components/Order/OrderManager/OrderManager.jsx
@@ -0,0 +1,32 @@
+OrderManager = React.createClass({
+
+    render() {
+        var order = this.props.order
+        return(<div className="mdl-grid">
+                    <div className="mdl-cell mdl-cell--4-col">
+                        <StatusForm order={order}/>
+                        <OrderContactForm order={order}/>
+                    </div>
+                    <div className="mdl-cell mdl-cell--4-col">
+                        <OrderEventForm order={order}/>
+                    </div>
+                    <div className="mdl-cell mdl-cell--4-col">
+                        <OrderAllergiesForm order={order}/>
+                        <OrderGeneralInputForm
+                            order={order}
+                            inputRows={1}
+                            inputName="extraInfo"
+                            inputLabel={T("order", "extra_form_label")}
+                            inputValue={order.extraInfo}
+                            inputErrorMsg={order.errors['extraInfo']}
+                        />
+                    </div>
+                    <div className="mdl-cell mdl-cell--4-col">
+                        <OrderMenuForm order={order}/>
+                    </div>
+                    <div className="mdl-cell mdl-cell--12-col">
+                        <MenuItemsContainer order={order}/>
+                    </div>
+                </div>)
+    }
+})
--- client/components/Order/OrderManager/OrderManagerContainer.jsx
+++ client/components/Order/OrderManager/OrderManagerContainer.jsx
@@ -14,32 +14,6 @@ OrderManagerContainer = React.createClass({
     render() {
         if (!this.data.subsReady)
             return <Loader/>
-        var order = this.data.order
-        return(<div className="mdl-grid">
-                    <div className="mdl-cell mdl-cell--4-col">
-                        <StatusForm order={order}/>
-                        <OrderContactForm order={order}/>
-                    </div>
-                    <div className="mdl-cell mdl-cell--4-col">
-                        <OrderEventForm order={order}/>
-                    </div>
-                    <div className="mdl-cell mdl-cell--4-col">
-                        <OrderAllergiesForm order={order}/>
-                        <OrderGeneralInputForm
-                            order={order}
-                            inputRows={1}
-                            inputName="extraInfo"
-                            inputLabel={T("order", "extra_form_label")}
-                            inputValue={order.extraInfo}
-                            inputErrorMsg={order.errors['extraInfo']}
-                        />
-                    </div>
-                    <div className="mdl-cell mdl-cell--4-col">
-                        <OrderMenuForm order={order}/>
-                    </div>
-                    <div className="mdl-cell mdl-cell--12-col">
-                        <MenuItemsContainer order={order}/>
-                    </div>
-                </div>)
+        return(<OrderManager order={this.data.order}/>)
     }
 })
