8ceb024946491207df257d464a529c62179d2917
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-12-01 13:35:43 +0200

    improve OrderManager layout

--- client/components/Order/OrderManager/OrderManagerContainer.jsx
+++ client/components/Order/OrderManager/OrderManagerContainer.jsx
@@ -15,21 +15,31 @@ OrderManagerContainer = React.createClass({
         if (!this.data.subsReady)
             return <Loader/>
         var order = this.data.order
-        return(<div>
-                <StatusForm order={order}/>
-                <OrderContactForm order={order}/>
-                <OrderEventForm order={order}/>
-                <OrderAllergiesForm order={order}/>
-                <OrderGeneralInputForm
-                    order={order}
-                    inputRows={1}
-                    inputName="extraInfo"
-                    inputLabel={T("order", "extra_form_label")}
-                    inputValue={order.extraInfo}
-                    inputErrorMsg={order.errors['extraInfo']}
-                />
-                <OrderMenuForm order={order}/>
-                <MenuItemsContainer order={order}/>
-            </div>)
+        return(<div className="mdl-grid">
+                    <div className="mdl-cell mdl-cell--4-col">
+                        <StatusForm order={order}/>
+                        <OrderContactForm order={order}/>
+                    </div>
+                    <div className="mdl-cell mdl-cell--4-col">
+                        <OrderEventForm order={order}/>
+                    </div>
+                    <div className="mdl-cell mdl-cell--4-col">
+                        <OrderAllergiesForm order={order}/>
+                        <OrderGeneralInputForm
+                            order={order}
+                            inputRows={1}
+                            inputName="extraInfo"
+                            inputLabel={T("order", "extra_form_label")}
+                            inputValue={order.extraInfo}
+                            inputErrorMsg={order.errors['extraInfo']}
+                        />
+                    </div>
+                    <div className="mdl-cell mdl-cell--4-col">
+                        <OrderMenuForm order={order}/>
+                    </div>
+                    <div className="mdl-cell mdl-cell--12-col">
+                        <MenuItemsContainer order={order}/>
+                    </div>
+                </div>)
     }
 })
