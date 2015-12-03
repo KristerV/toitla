95a7b0aacfbe37c819da41cecc7e344895d4ab5c
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-12-03 20:06:20 +0200

    move backup script to JS

--- server/mongoBackupCron.js
+++ server/mongoBackupCron.js
@@ -1,3 +1,3 @@
 Meteor.setTimeout(function(){
-     Shelljs.exec('sh /srv/toitlalive/private/backup_server_nossh.sh')
+     Shelljs.exec('docker exec -it mongodb mongodump -o dump/$(date +%F--%H-%M)/ && docker cp mongodb:dump/$(date +%F--%H-%M) /srv/backups')
 }, 1000 * 60*2)// * 60 * 4);
