f5cec5d91a9cbd5d4063b6fb6e28f0b75e254c96
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-12-03 20:26:44 +0200

    correct backup cron interval

--- server/mongoBackupCron.js
+++ server/mongoBackupCron.js
@@ -2,4 +2,4 @@ Meteor.setInterval(function(){
     result2 = Shelljs.exec('docker exec -it mongodb mongodump -o dump/$(date +%F--%H-%M)/ && docker cp mongodb:dump/$(date +%F--%H-%M) /srv/backups', function(code, output){
         console.log("BACKUP", code, output);
     })
-}, 1000 * 5)// * 60 * 4);
+}, 1000 * 60 * 60 * (24 / 4));
