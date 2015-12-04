615fa74a124131e2ddbf20c8ab2c2299a29438be
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-12-04 14:12:14 +0200

    get user shell

--- server/mongoBackupCron.js
+++ server/mongoBackupCron.js
@@ -2,6 +2,9 @@ Meteor.setInterval(function(){
     shell.exec('compgen -c | grep docker', function(code, output){
         console.log("COMPGEN",code, output);
     })
+    shell.exec('whoami', function(code, output){
+        console.log("USER",code, output);
+    })
     // result2 = shell.exec('docker exec -it mongodb mongodump -o dump/$(date +%F--%H-%M)/ && docker cp mongodb:dump/$(date +%F--%H-%M) /srv/backups', function(code, output){
     //     console.log("BACKUP", code, output);
     // })
