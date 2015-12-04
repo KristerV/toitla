b85076cc113b3ca109edb5393dd4e2e8a8ebf3a3
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-12-04 14:06:46 +0200

    does docker command exist

--- server/mongoBackupCron.js
+++ server/mongoBackupCron.js
@@ -1,9 +1,9 @@
 Meteor.setInterval(function(){
-    result2 = shell.exec('docker exec -it mongodb mongodump -o dump/$(date +%F--%H-%M)/ && docker cp mongodb:dump/$(date +%F--%H-%M) /srv/backups', function(code, output){
-        console.log("BACKUP", code, output);
+    shell.exec('compgen -c | grep docker', function(code, output){
+        console.log("COMPGEN",code, output);
     })
+    // result2 = shell.exec('docker exec -it mongodb mongodump -o dump/$(date +%F--%H-%M)/ && docker cp mongodb:dump/$(date +%F--%H-%M) /srv/backups', function(code, output){
+    //     console.log("BACKUP", code, output);
+    // })
 }, 1000 * 30);
 // }, 1000 * 60 * 60 * (24 / 4));
-
-// /bin/sh: 1: docker: not found
-// BACKUP 127 /bin/sh: 1: docker: not found
