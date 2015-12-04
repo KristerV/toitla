2319397249259cb633ad5137e408ea6788488015
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-12-04 10:10:52 +0200

    disable cron, toitla does not work

--- server/mongoBackupCron.js
+++ server/mongoBackupCron.js
@@ -1,5 +1,5 @@
-Meteor.setInterval(function(){
-    result2 = Shelljs.exec('docker exec -it mongodb mongodump -o dump/$(date +%F--%H-%M)/ && docker cp mongodb:dump/$(date +%F--%H-%M) /srv/backups', function(code, output){
-        console.log("BACKUP", code, output);
-    })
-}, 1000 * 60 * 60 * (24 / 4));
+// Meteor.setInterval(function(){
+//     result2 = Shelljs.exec('docker exec -it mongodb mongodump -o dump/$(date +%F--%H-%M)/ && docker cp mongodb:dump/$(date +%F--%H-%M) /srv/backups', function(code, output){
+//         console.log("BACKUP", code, output);
+//     })
+// }, 1000 * 60 * 60 * (24 / 4));
