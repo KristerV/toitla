aae000e938a441bed18cf9734a158f2abeb96d8d
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-01-15 11:57:44 +0200

    add email link on login

--- client/components/Login/LoginForm.jsx
+++ client/components/Login/LoginForm.jsx
@@ -1,11 +1,19 @@
 LoginForm = React.createClass({
 
+    getInitialState() {
+        return {
+            emailDomain: null,
+        }
+    },
+
     sendLink(e) {
         e.preventDefault()
         var email = e.target[0].value
         if (Security.isEmail(email)) {
             Meteor.call('sendEmailLogin', email)
             $('input#email').val("")
+            var emailDomain = email.split("@")[1]
+            this.setState({emailDomain: emailDomain})
             this.showSent()
         } else {
             this.showNotEmail()
@@ -41,6 +49,7 @@ LoginForm = React.createClass({
                         ref="email"
                     />
                     <button id="login" type="submit" className="button-login-send mdl-button mdl-js-button mdl-button--raised mdl-button--accent w100 mdl-js-ripple-effect"><span>login / register</span></button>
+                    {this.state.emailDomain ? <p style={{paddingTop: "1em"}}>Now go check your email at <a target="_blank" href={"http://" + this.state.emailDomain}>{this.state.emailDomain}</a></p> : null}
                 </form>
                 <p className="text-white text-center margin">
                     Passwords are not used here. Instead, you'll get an access link in your email. No need to remember another password.
