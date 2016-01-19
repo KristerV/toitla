df7bc0461ef4322aa977f73a0ac18a1f516d2c96
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-01-19 12:15:24 +0200

    remove commas in order prices too

--- server/runOnce.js
+++ server/runOnce.js
@@ -1,13 +1,20 @@
 // Convert price from , to .
-// Meteor.startup(function(){
-//     var items = MenuitemTemplates.find()
-//     items.forEach(function(item){
-//         if (item.price && !Number(item.price) && item.price.indexOf(',') !== -1) {
-//             var newPrice = Number(item.price.replace(/,/g , "."))
-//             MenuitemTemplates.update(item._id, {$set: {price: newPrice}})
-//         }
-//     })
-// });
+Meteor.startup(function(){
+    var items = MenuitemsInOrder.find()
+    items.forEach(function(item){
+        if (item.price && !Number(item.price) && item.price.indexOf(',') !== -1) {
+            var newPrice = Number(item.price.replace(/,/g , "."))
+            MenuitemsInOrder.update(item._id, {$set: {price: newPrice}})
+        }
+    })
+    var items = MenuitemTemplates.find()
+    items.forEach(function(item){
+        if (item.price && !Number(item.price) && item.price.indexOf(',') !== -1) {
+            var newPrice = Number(item.price.replace(/,/g , "."))
+            MenuitemTemplates.update(item._id, {$set: {price: newPrice}})
+        }
+    })
+});
 
 // Convert priceClass to freetext price
 // Meteor.startup(function(){
