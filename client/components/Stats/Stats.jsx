0fa69f000be893963af6efc415f47dcfa51b54e5
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-01-25 19:56:30 +0200

    feedback is also done

--- client/components/Stats/Stats.jsx
+++ client/components/Stats/Stats.jsx
@@ -18,7 +18,7 @@ Stats = React.createClass({
             var dateString = moment(order.event.fromDate).format('DD.MM.YY')
             if (order.status.phase === 'lost')
                 data[dateString].lost++
-            else if (order.status.phase === 'done') {
+            else if (order.status.phase === 'done' || order.status.phase === 'feedback') {
                 data[dateString].done++
                 data[dateString].total++
             } else {
