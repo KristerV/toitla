aaedd0d5acc18e09f6fe84e0ef441359fde7afa4
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-02-04 18:45:52 +0200

    remove useless wrapper package

--- server/SSOAPI.js
+++ server/SSOAPI.js
@@ -1,5 +1,6 @@
 Meteor.methods({
     discourseSSO:function(userId, payload, sig){
+        discourse_sso = Npm.require('discourse-sso');
 
         if (!userId)
             throw new Meteor.Error(401, "Not logged in. Please log into Toitla before going to the forum.")
