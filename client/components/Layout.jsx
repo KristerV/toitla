8d9822b7516f40008999d4799c06a70a46dde138
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-02-07 18:07:36 +0200

    transparent navbar in mobile is much cooler

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -44,23 +44,23 @@ Layout = React.createClass({
 
         // wrapper div fixes issue: https://github.com/facebook/react-devtools/issues/273
         return(<div>
-            <div className={"mdl-layout mdl-js-layout mdl-layout--fixed-header"}>
+            <div className={"mdl-layout mdl-js-layout"}>
                 <header className="mdl-layout__header mdl-layout--no-desktop-drawer-button">
                     <div className="mdl-layout__header-row mdl-layout--large-screen-only">
                         {title}
                         <div className="mdl-layout-spacer"></div>
                         <nav className="mdl-navigation">{links}</nav>
                     </div>
-                    <div className="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
-                        <div className="mdl-tabs__tab-bar" style={{height: "auto", borderBottom: "none"}}>
-                            {_.map(this.props.tabs, function(tab, i){
-                                var newPath = currentPath.replace(this.props.activeTab, tab.route)
-                                var isActive = tab.route === this.props.activeTab ? 'is-active' : ""
-                                return <a key={i} href={newPath} className={"mdl-layout__tab "+isActive}>{tab.label}</a>
-                            }.bind(this))}
-                        </div>
-                    </div>
                 </header>
+                <div className={"mdl-tabs mdl-js-tabs mdl-js-ripple-effect " + (this.props.tabs ? "mdl-layout__header" : "")}>
+                    <div className="mdl-tabs__tab-bar" style={{height: "auto", borderBottom: "none"}}>
+                        {_.map(this.props.tabs, function(tab, i){
+                            var newPath = currentPath.replace(this.props.activeTab, tab.route)
+                            var isActive = tab.route === this.props.activeTab ? 'is-active' : ""
+                            return <a key={i} href={newPath} className={"mdl-layout__tab "+isActive}>{tab.label}</a>
+                        }.bind(this))}
+                    </div>
+                </div>
                 <div className="mdl-layout__drawer">
                     {title}
                     <nav className="mdl-navigation">{links}</nav>
