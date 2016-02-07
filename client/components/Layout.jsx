76d1f1ce7fbf4e7eff29d0b6e98e39ae74922afd
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-02-07 17:49:02 +0200

    dynamic tabs kept making main nav disappear

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -41,26 +41,25 @@ Layout = React.createClass({
 
         var title = <a className="mdl-navigation__link mdl-layout-title" style={{fontSize: "1.6rem"}} href="/home">Toitla</a>
         var currentPath = FlowRouter.current().path
-        var isTabs = this.props.tabs
 
         // wrapper div fixes issue: https://github.com/facebook/react-devtools/issues/273
         return(<div>
-            <div className={"mdl-layout mdl-js-layout " + (isTabs ? 'mdl-layout--fixed-header':"")}>
+            <div className={"mdl-layout mdl-js-layout mdl-layout--fixed-header"}>
                 <header className="mdl-layout__header mdl-layout--no-desktop-drawer-button">
                     <div className="mdl-layout__header-row mdl-layout--large-screen-only">
                         {title}
                         <div className="mdl-layout-spacer"></div>
                         <nav className="mdl-navigation">{links}</nav>
                     </div>
-                    {isTabs ?
-                        <div className="">
+                    <div className="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
+                        <div className="mdl-tabs__tab-bar" style={{height: "auto", borderBottom: "none"}}>
                             {_.map(this.props.tabs, function(tab, i){
                                 var newPath = currentPath.replace(this.props.activeTab, tab.route)
                                 var isActive = tab.route === this.props.activeTab ? 'is-active' : ""
                                 return <a key={i} href={newPath} className={"mdl-layout__tab "+isActive}>{tab.label}</a>
                             }.bind(this))}
                         </div>
-                    : null}
+                    </div>
                 </header>
                 <div className="mdl-layout__drawer">
                     {title}
