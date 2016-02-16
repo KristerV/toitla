9b8a640f44e1f8a59e6bea15996d2bf66cd43f82
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-02-16 15:56:39 +0200

    disable order status checklist

--- client/components/Order/OrderManager/OrderManagerStatus.jsx
+++ client/components/Order/OrderManager/OrderManagerStatus.jsx
@@ -2,6 +2,7 @@ OrderManagerStatus = React.createClass({
     render() {
         return(<div className="max-width margin-top mdl-grid">
             <Checklist
+                disabled={true}
                 collectionName="orders"
                 docId={this.props.order._id}
                 datapath="status"
