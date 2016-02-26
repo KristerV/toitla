9481e623aa3aeb145e80f18812cac18273fcc229
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-02-26 16:23:39 +0200

    switch from mandrill to smtp site5

--- server/email.js
+++ server/email.js
@@ -1,49 +1,22 @@
-Email = {
-  send: function(from, to, subject, html, fromName) {
-      HTTP.post('https://mandrillapp.com/api/1.0/messages/send.json',
-        {
-          "data": {
-            "key": process.env.MANDRILL_KEY,
-            "message": {
-              "from_email": from || 'teavitus@toitla.com',
-              "from_name": 'Toitla',
-              "subject": subject,
-              "html": html,
-              "to": [
-                {
-                  "email": to,
-                  "type": "to"
-                }
-              ],
-            }
-          }
-        },
-        function(error, result) {
-          if(error){
-            console.error("Email error: " + error)
-          } else {
-            console.info("Email sent")
-          }
-        }
-      )
-  }
-}
-
 Meteor.methods({
-  sendEmail:function(from, to, subject, html){
-    check(from, String)
-    check(to, String)
-    check(subject, String)
-    check(html, String)
-    if (Roles.userIsInRole(this.userId, 'manager')) {
-      Email.send(from, to, subject, html)
-    } else {
-      throw new Meteor.Error("You're not allowed to send email")
-    }
+    sendEmail: function (from, to, subject, html) {
+        check([from, to], [Match.OneOf(String, null)])
+        check([html, subject], [String])
+        if (Meteor.userId()) {
+            Email.send({
+                from: from || Settings.system_email,
+                to: to,
+                bcc: Settings.toitla.fleepConvo,
+                subject: subject,
+                html: html
+            })
+        } else {
+            throw new Meteor.Error("You're not allowed to send email")
+        }
 
-  },
-  sendEmailLogin: function(email) {
-      email = email.replace(/ /g,'')
-      Accounts.sendLoginEmail(email)
-  }
+    },
+    sendEmailLogin: function (email) {
+        email = email.replace(/ /g, '')
+        Accounts.sendLoginEmail(email)
+    }
 });
--- server/sendLoginEmail.js
+++ server/sendLoginEmail.js
@@ -1,37 +1,37 @@
-Accounts.sendLoginEmail = function(address){
+Accounts.sendLoginEmail = function (address) {
 
-  if (! address) {
-    throw new Meteor.Error("No email provided")
-  } else if (!/^[^@]+@[^@]+\.[^@]+$/.test(address)) {
-    throw new Meteor.Error("Provided string is not an email")
-  }
+    if (!address) {
+        throw new Meteor.Error("No email provided")
+    } else if (!/^[^@]+@[^@]+\.[^@]+$/.test(address)) {
+        throw new Meteor.Error("Provided string is not an email")
+    }
 
-  var user = Accounts.findUserByEmail(address)
+    var user = Accounts.findUserByEmail(address)
 
-  if (! user) {
-    Accounts.createUser({email: address})
-    user = Accounts.findUserByEmail(address)
-  }
+    if (!user) {
+        Accounts.createUser({email: address})
+        user = Accounts.findUserByEmail(address)
+    }
 
-  var tokenRecord = Accounts._generateStampedLoginToken()
-  Accounts._insertLoginToken(user._id, tokenRecord);
+    var tokenRecord = Accounts._generateStampedLoginToken()
+    Accounts._insertLoginToken(user._id, tokenRecord);
 
-  var loginUrl = Meteor.absoluteUrl('login/' + tokenRecord.token)
-  console.info(loginUrl);
+    var loginUrl = Meteor.absoluteUrl('login/' + tokenRecord.token)
+    console.info(loginUrl);
 
-  var html = ''
-  if (user.profile) {
-    html += '<p>Hey ' + user.profile.name + ',</p>'
-    html += "<p>To login, just follow this link.</p>"
-  }
-  else {
-    html += '<p>Hey there,</p>'
-    html += "<p>To register, just follow this link. A user wil be created for you.</p>"
-  }
+    var html = ''
+    if (user.profile) {
+        html += '<p>Hey ' + user.profile.name + ',</p>'
+        html += "<p>To login, just follow this link.</p>"
+    }
+    else {
+        html += '<p>Hey there,</p>'
+        html += "<p>To register, just follow this link. A user wil be created for you.</p>"
+    }
 
-  html += '<p><a target="_blank" href="'+loginUrl+'">'+loginUrl+'</a></p>'
+    html += '<p><a target="_blank" href="' + loginUrl + '">' + loginUrl + '</a></p>'
 
-  html += '<p>All the best, Toitla</p>'
+    html += '<p>All the best, Toitla</p>'
 
-  Email.send(null, address, 'Toitla login link', html)
+    Meteor.call('sendEmail', null, address, 'Toitla login link', html)
 };
