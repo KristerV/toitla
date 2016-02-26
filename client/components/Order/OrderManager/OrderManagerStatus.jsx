aea51819dcd94a9f25200c46717c430122497730
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-02-26 15:40:46 +0200

    restructure && cancel accidental result

--- client/components/Order/OrderManager/OrderManagerStatus.jsx
+++ client/components/Order/OrderManager/OrderManagerStatus.jsx
@@ -1,25 +1,11 @@
 OrderManagerStatus = React.createClass({
 
-    orderDone() {
-        Meteor.call('Order--resultChange', this.props.order._id, 'done')
-    },
-    orderLost() {
-        Meteor.call('Order--resultChange', this.props.order._id, 'lost')
-    },
-
     submitOrder() {
         Meteor.call('NewOrder.submitForm', this.props.order._id)
     },
 
     render() {
         var order = this.props.order
-        order.result = order.result || {}
-
-        var isDone = order.result.result === 'done'
-        var isLost = order.result.result === 'lost'
-
-        var doneClass = isDone ? " mdl-button--accent" : ""
-        var lostClass = isLost ? " mdl-button--colored" : ""
 
         return(<div className="max-width margin-top mdl-grid">
             <div className="mdl-cell mdl-cell--4-col">
@@ -41,30 +27,7 @@ OrderManagerStatus = React.createClass({
             </div>
             <div className="mdl-cell mdl-cell--4-col">
                 <h3 className="text-white text-center">Result</h3>
-                <div className="paper padding mdl-grid">
-                    <button className={"mdl-button mdl-js-button mdl-button--raised mdl-cell mdl-cell--6-col" + doneClass} onClick={this.orderDone}>
-                        Done
-                    </button>
-                    <button className={"mdl-button mdl-js-button mdl-button--raised mdl-cell mdl-cell--6-col" + lostClass} onClick={this.orderLost}>
-                        Lost
-                    </button>
-                    {isDone ?
-                        <TextInput
-                            label="How many pieces of food was left over?"
-                            name="result.foodLeft"
-                            value={order.result.foodLeft}
-                            patternTemplate="float"
-                            onBlur={order.handleTextFieldChange.bind(order)}
-                        />
-                    : null}
-                    {isLost ?
-                        <Checklist
-                            datapath="lostReasons"
-                            collectionName="orders"
-                            docId={order._id}
-                        />
-                    : null}
-                </div>
+                <OrderResult order={order}/>
             </div>
         </div>)
     }
---
+++ client/components/Order/OrderManager/OrderResult.jsx
@@ -0,0 +1,53 @@
+OrderResult = React.createClass({
+
+    handleClickResult(e) {
+        const btn = e.target.name
+        const current = this.props.order.result.result
+        let newStatus = ""
+        if (btn === current)
+            newStatus = null
+        else
+            newStatus = btn
+        Meteor.call('Order--resultChange', this.props.order._id, newStatus)
+    },
+
+    render() {
+        var order = this.props.order
+        order.result = order.result || {}
+
+        var isDone = order.result.result === 'done'
+        var isLost = order.result.result === 'lost'
+
+        var doneClass = isDone ? " mdl-button--accent" : ""
+        var lostClass = isLost ? " mdl-button--colored" : ""
+        return (<div className="paper padding mdl-grid">
+            <button name="done"
+                className={"mdl-button mdl-js-button mdl-button--raised mdl-cell mdl-cell--6-col" + doneClass}
+                onClick={this.handleClickResult}>
+                Done
+            </button>
+            <button name="lost"
+                className={"mdl-button mdl-js-button mdl-button--raised mdl-cell mdl-cell--6-col" + lostClass}
+                onClick={this.handleClickResult}>
+                Lost
+            </button>
+            {isDone ?
+                <TextInput
+                    label="How many pieces of food was left over?"
+                    name="result.foodLeft"
+                    value={order.result.foodLeft}
+                    patternTemplate="float"
+                    onBlur={order.handleTextFieldChange.bind(order)}
+                />
+                : null}
+            {isLost ?
+                <Checklist
+                    datapath="lostReasons"
+                    collectionName="orders"
+                    docId={order._id}
+                />
+                : null}
+        </div>)
+    }
+
+})
