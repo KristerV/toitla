bc30e76c2a1b6134731131c09ae8577333c1bcaf
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-03-21 15:13:33 +0200

    simpler html

--- client/components/Landing/Landing.jsx
+++ client/components/Landing/Landing.jsx
@@ -33,8 +33,8 @@ Landing = React.createClass({
                 chefs.push(user)
         })
 
-        return (<div className="landing">
-            <section className="mdl-grid text-center bg-temp shadow-bottom">
+        return (<div className="landing text-center">
+            <section className="mdl-grid bg-temp shadow-bottom">
                 <div className="mdl-cell mdl-cell--6-col text-white">
                     <Toitla white={true} shadow={true} size={1.4}/>
                     <h3 style={{fontFamily: "GH"}} className="text-shadow">{T.landing.description()}</h3>
@@ -42,7 +42,7 @@ Landing = React.createClass({
                 </div>
                 <div className="mdl-cell mdl-cell--6-col">
                     <div className="paper center padding" style={{maxWidth: "300px"}}>
-                        <h4 className="text-center">{T.order.form.initial_heading()}</h4>
+                        <h4>{T.order.form.initial_heading()}</h4>
                         <TextInput label={T.order.form.nrPeople()} />
                         <TextInput label={T.order.form.duration()} />
                         <TextInput label={T.order.form.contact_email()} />
@@ -54,17 +54,17 @@ Landing = React.createClass({
             <section>
                 <div className="landing-degustation center mdl-grid paper no-padding">
                     <div className="mdl-cell mdl-cell--8-col no-margin bg-temp"></div>
-                    <div className="mdl-cell mdl-cell--4-col text-center">
+                    <div className="mdl-cell mdl-cell--4-col">
                         <h5>{T.landing.degustation_title()}</h5>
                         <Button label={T.landing.degustation_button()} raised={true}/>
                     </div>
                 </div>
             </section>
 
-            <section className="text-center">
+            <section>
                 <h2>We've served <span style={{fontSize: "2em"}}>72</span> events</h2>
                 <h4>Our largest event was <span style={{fontSize: "2em"}}>160</span> people</h4>
-                <div className="text-center max-width">
+                <div className="max-width">
                     {Settings.landing.client_logos.map(filename => {
                         return <div key={filename} className="center" style={{padding: "0 2em" ,width: "8em", height: "6em", display: "inline-block"}}>
                             <Imgix path="/images/landing/clients" filename={filename} fit="clip"/>
@@ -74,7 +74,7 @@ Landing = React.createClass({
             </section>
 
             <section>
-                <h1 className="text-center">Some of our servings</h1>
+                <h1>Some of our servings</h1>
                 <div className="mdl-grid max-width">
                     {settings.eventImages.map((fullpath, i) => {
                         return <div className="mdl-cell mdl-cell--4-col paper" key={i}>
@@ -111,9 +111,9 @@ Landing = React.createClass({
                 <p>Kokad valitakse ja valideeritakse käsitsi. Hetkel on neid üle 15-ne.</p>
                 <div className="mdl-grid max-width">
                     {chefs.map((user, i) => {
-                        return <div className="mdl-cell mdl-cell--2-col padding" key={i}>
+                        return <div className="mdl-cell mdl-cell--2-col padding center" key={i}>
                             <ProfileImage user={user}/>
-                            <p className="text-center">{user.profile.name}</p>
+                            <p>{user.profile.name}</p>
                         </div>
                     })}
                 </div>
@@ -124,9 +124,9 @@ Landing = React.createClass({
                 <p>Alustasime oktoober 2014 blaablaa</p>
                 <div className="mdl-grid max-width">
                     {admins.map((user, i) => {
-                        return <div className="mdl-cell mdl-cell--2-col padding" key={i}>
+                        return <div className="mdl-cell mdl-cell--2-col padding center" key={i}>
                             <ProfileImage user={user}/>
-                            <p className="text-center">{user.profile.name}</p>
+                            <p>{user.profile.name}</p>
                         </div>
                     })}
                 </div>
