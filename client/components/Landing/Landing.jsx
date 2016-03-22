1a7da4f92361280a3135b840a3ba6df691205b3b
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-03-22 12:54:36 +0200

    landing translations

--- client/components/Landing/Landing.jsx
+++ client/components/Landing/Landing.jsx
@@ -38,6 +38,7 @@ Landing = React.createClass({
     },
 
     openChat(e) {
+        console.log("Landing.openChat")
         e.preventDefault()
         Tawk_API.maximize();
     },
@@ -59,8 +60,8 @@ Landing = React.createClass({
             <section className="mdl-grid shadow-bottom relative imgix-fluid imgix-fluid-bg" data-src={Settings.landing.mainImage}>
                 <div className="mdl-cell mdl-cell--6-col text-white">
                     <Toitla white={true} shadow={true} size={1.4}/>
-                    <h3 style={{fontFamily: "GH"}} className="text-shadow">{T.landing.description()}</h3>
-                    <Button label={T.landing.main_button()} raised={true} colored={true} onClick={this.goToHowTo}/>
+                    <h3 style={{fontFamily: "GH"}} className="text-shadow">{T.landing.hero.description()}</h3>
+                    <Button label={T.landing.ordernow_button()} raised={true} colored={true} onClick={this.goToHowTo}/>
                 </div>
                 <div className="mdl-cell mdl-cell--6-col" id="orderNow">
                     <div className="paper center padding" style={{maxWidth: "300px"}}>
@@ -79,15 +80,15 @@ Landing = React.createClass({
                         <Imgix path="images/landing/features" filename="surprise.jpg" height={230}/>
                     </div>
                     <div className="mdl-cell mdl-cell--4-col">
-                        <h5>{T.landing.degustation_title()}</h5>
-                        <Button label={T.landing.degustation_button()} raised={true}/>
+                        <h5>{T.landing.sampleBox.title()}</h5>
+                        <Button label={T.landing.sampleBox.button()} raised={true}/>
                     </div>
                 </div>
             </section>
 
             <section>
-                <h2>We've served <span style={{fontSize: "2em"}}>72</span> events</h2>
-                <h4>Our largest event was <span style={{fontSize: "2em"}}>160</span> people</h4>
+                <h2 dangerouslySetInnerHTML={T.landing.score.nr_events({dangerous: true})}></h2>
+                <h4 dangerouslySetInnerHTML={T.landing.score.nr_people({dangerous: true})}></h4>
                 <div className="max-width">
                     {Settings.landing.client_logos.map(filename => {
                         return <div key={filename} className="center" style={{padding: "0 2em" ,width: "8em", height: "6em", display: "inline-block"}}>
@@ -98,7 +99,7 @@ Landing = React.createClass({
             </section>
 
             <section>
-                <h1>Some of our servings</h1>
+                <h1>{T.landing.score.gallery()}</h1>
                 <div className="mdl-grid max-width">
                     {settings.eventImages.map((fullpath, i) => {
                         return <div className="mdl-cell mdl-cell--4-col no-margin" key={i}>
@@ -109,14 +110,14 @@ Landing = React.createClass({
             </section>
 
             <section id="howto">
-                <h1>How to order</h1>
+                <h1>{T.landing.howto.title()}</h1>
                 <ol className="center how-to-order" style={{maxWidth: "400px"}}>
-                    <li>Fill form</li>
-                    <h6 className="text-hint">The form asks all the information we need to build you a custom menu.</h6>
-                    <li>Confirm menu or make changes</li>
-                    <h6 className="text-hint">If you're satisfied with the menu we're all set and will be serving at your event.</h6>
-                    <li>Bill and feedback</li>
-                    <h6 className="text-hint">We'll send the bill and would love to hear any feedback you may have.</h6>
+                    <li>{T.landing.howto.first()}</li>
+                    <h6 className="text-hint">{T.landing.howto.first_sub()}</h6>
+                    <li>{T.landing.howto.second()}</li>
+                    <h6 className="text-hint">{T.landing.howto.second_sub()}</h6>
+                    <li>{T.landing.howto.third()}</li>
+                    <h6 className="text-hint">{T.landing.howto.third_sub()}</h6>
                 </ol>
                 {this.getOrderNowButton()}
             </section>
@@ -130,8 +131,8 @@ Landing = React.createClass({
                                 <Imgix filename={item.img} />
                             </div>
                             <div className="mdl-cell mdl-cell--8-col">
-                                <h5>{item.title}</h5>
-                                <p>{item.text}</p>
+                                <h5>{T.landing.features[item.title]()}</h5>
+                                <p>{T.landing.features[item.text]()}</p>
                             </div>
                         </div>
                     })}
@@ -139,8 +140,8 @@ Landing = React.createClass({
             </section>
 
             <section>
-                <h1>Kokad</h1>
-                <h5>Kokad valitakse ja valideeritakse käsitsi. Hetkel on neid üle 15-ne.</h5>
+                <h1>{T.landing.users.chefs()}</h1>
+                <h5>{T.landing.users.chefs_text()}</h5>
                 <div className="mdl-grid max-width">
                     {chefs.map((user, i) => {
                         return <div className="mdl-cell mdl-cell--2-col padding center" key={i}>
@@ -152,8 +153,8 @@ Landing = React.createClass({
             </section>
 
             <section>
-                <h1>Meie</h1>
-                <h5>Alustasime oktoober 2014 blaablaa</h5>
+                <h1>{T.landing.users.team()}</h1>
+                <h5>{T.landing.users.team_text()}</h5>
                 <div className="mdl-grid max-width">
                     {admins.map((user, i) => {
                         return <div className="mdl-cell mdl-cell--2-col padding center" key={i}>
@@ -165,14 +166,14 @@ Landing = React.createClass({
             </section>
 
             <section>
-                <h2>More questions?</h2>
-                <h6><a onClick={this.openChat} href="#" >Click here</a> to chat with us or email us at <a target="_blank" href="mailto:team@toitla.com">team@toitla.com</a></h6>
+                <h2>{T.landing.final.title()}</h2>
+                <h6 dangerouslySetInnerHTML={T.landing.final.text({dangerous: true})}></h6>
                 {this.getOrderNowButton()}
             </section>
         </div>)
     },
 
     getOrderNowButton() {
-        return <Button label="Order Now" accent={true} raised={true} onClick={this.goToForm} large={true} />
+        return <Button label={T.landing.ordernow_button()} accent={true} raised={true} onClick={this.goToForm} large={true} />
     }
 })
