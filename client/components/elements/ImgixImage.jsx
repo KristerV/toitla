8b2310a8742dc0e6c690295515059e1d2b7867e7
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-04-19 13:04:54 +0300

    remove white stripes between images

--- client/components/elements/ImgixImage.jsx
+++ client/components/elements/ImgixImage.jsx
@@ -12,16 +12,17 @@ ImgixImage = React.createClass({
     render() {
         var width = this.props.width || "100%"
         var padBott = this.props.height ? this.props.height+"px" : "100%"
+        var style = {width: width,
+            backgroundSize: "contain",
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: "center"
+        }
+        _.extend(style, this.props.style, {paddingBottom: padBott}) // overwrite defaults with props (padBott is important)
         return<div
             id={this.props.id}
             data-src={this.props.src}
             className={"imgix-fluid imgix-fluid-bg " + this.props.className}
-            style={{width: width,
-                backgroundSize: "contain",
-                backgroundRepeat: 'no-repeat',
-                paddingBottom: padBott,
-                backgroundPosition: "center"
-                }}
+            style={style}
         ></div>
     }
 
