ae88ce25504e25224d44ef990bbbb0abdc091e06
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-04-21 22:28:18 +0300

    hack images to work

--- client/lib/imgix.init.js
+++ client/lib/imgix.init.js
@@ -1,19 +1,23 @@
 Meteor.startup(() => {
     imgix.onready(function() {
-        imgix.fluid({
-            updateOnResizeDown: false,
-            pixelStep: 10,
-            autoInsertCSSBestPractices: false,
-            onChangeParamOverride: function(w, h, params, element) {
 
-                if (params.shape === 'square') {
-                    const min = Math.min(w, h) || w || h
-                    params.w = `${min}`
-                    params.h = `${min}`
+        // HACK: Imgix will load too soon and images will not load (will need change of resolution)
+        Meteor.setTimeout(() => {
+            imgix.fluid({
+                updateOnResizeDown: false,
+                pixelStep: 10,
+                autoInsertCSSBestPractices: false,
+                onChangeParamOverride: function(w, h, params, element) {
+
+                    if (params.shape === 'square') {
+                        const min = Math.min(w, h) || w || h
+                        params.w = `${min}`
+                        params.h = `${min}`
+                    }
+
+                    return params;
                 }
-
-                return params;
-            }
-        });
+            });
+        }, 1000)
     });
 })
