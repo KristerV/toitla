677167630a65c29f86085997897b10f0a38f6efe
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-06-27 21:32:23 +0300

    restore live server scripts

--- private/restore_local_from_live.sh
+++ private/restore_local_from_live.sh
@@ -10,3 +10,7 @@ if [ ! -d "../backups/$(date +%F)" ]; then
 fi
 
 mongorestore -h 127.0.0.1 --port 3001 --db meteor --drop ../backups/$(ls -t ../backups/ | head -1)/toitlalive/
+
+### Restoring to server https://github.com/arunoda/meteor-up/issues/816#issuecomment-174391839
+## Copy backups from local to server
+# scp -r /home/krister/code/backups/2016-06-20/toitlalive/ root@178.62.220.180:/srv/backups/2016-06-20/toitlalive/
