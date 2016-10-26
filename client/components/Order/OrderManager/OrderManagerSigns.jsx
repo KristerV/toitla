a5234db8ccc1fa43247038026647f44f0a7d857e
Trevor Himma <trevor@snowhound.eu>
2016-10-26 20:57:10 +0300

    Print menu items to A6 papers; removed sign of chefs

--- client/components/Order/OrderManager/OrderManagerSigns.jsx
+++ client/components/Order/OrderManager/OrderManagerSigns.jsx
@@ -1,80 +1,38 @@
 import React from 'react';
 OrderManagerSigns = React.createClass({
 
-    getInitialState() {
-        return {
-            printMainCard: true
-        }
-    },
-
     mixins: [ReactMeteorData],
     getMeteorData() {
-        var subscription = Meteor.subscribe("menuitems_inorder", this.props.orderId)
-        var menuitems = MenuitemsInOrder.find({orderId: this.props.order._id})
+        var subscription = Meteor.subscribe("menuitems_inorder", this.props.orderId);
+        var menuItems = MenuitemsInOrder.find({orderId: this.props.order._id});
 
         return {
-            menuitems: menuitems.fetch(),
+            menuItems: menuItems.fetch(),
             subsReady: subscription.ready()
         }
     },
 
-    changeMainCardPrint(e) {
-        this.setState({printMainCard: e.target.checked})
-    },
-
     render() {
-        if (!this.data.subsReady)
-            return <Loader/>
-        var menuitems = this.data.menuitems
-
-        let chefsUniq = []
-        chefs = []
-        menuitems.forEach(item => {
-            if (!_.contains(chefsUniq, item.chefId)) {
-                chefs.push({_id: item.chefId, name: item.chefName})
-                chefsUniq.push(item.chefId)
-            }
-        })
-
-        var rows = 2
-        var cols = 2
-        var backFace = true
-
-        var signWidth = (100 / cols) + "%"
-        var signHeight = (100 / rows) + "%"
-
-        var pages = []
-        var nrPerPage = rows * cols
-        var nrPages = Math.ceil(menuitems.length / nrPerPage)
-        let counter = 0
-        for (var i = 0; i < nrPages; i++) {
-            var pageContent = []
-            for (var j = 0; j < nrPerPage; j++) {
-                var item = menuitems[counter++]
-                if (!item) break
-                pageContent.push(<div key={item._id} style={{width: signWidth, height: signHeight, display: "inline-block"}}>
-                    <Sign menuitem={item} backFace={backFace}/>
-                </div>)
-            }
-            pages.push(<div key={i} className="sign-page center">{pageContent}</div>)
+        if (!this.data.subsReady) {
+            return <Loader/>;
         }
+
+        var pages = [];
+
+        this.data.menuItems.forEach(item => {
+            pages.push(<div key={item._id + 'wrapper'} className="sign-page"><Sign key={item._id} menuitem={item} backFace={true}/></div>);
+        });
 
         return(<div className="sign-layout max-width margin-top margin-bottom">
             <h3 className="text-center text-white">Printing center</h3>
             <div className="center paper padding" style={{width: "200px"}}>
-                <Checkbox
-                    onChange={this.changeMainCardPrint}
-                    label="Print main card"
-                    checked={this.state.printMainCard}
-                />
                 <button className="w100 mdl-button mdl-button--raised mdl-button--accent" onClick={G.print}>Print menu</button>
             </div>
-            <div id="printable-container">
+            <div id="printable-container" style={{padding: "40px 80px"}}>
                 <div id="printable-content">
-                    {this.state.printMainCard ? <SignToitla chefs={chefs}/> : null}
                     {pages}
                 </div>
             </div>
         </div>)
     }
-})
+});
