b446bb9f3cbed13ae8d13ecd250a083254c7334e
Trevor Himma <trevor@snowhound.eu>
2016-11-10 20:47:47 +0200

    Changed login email content

--- server/sendLoginEmail.js
+++ server/sendLoginEmail.js
@@ -6,37 +6,48 @@ Accounts.sendLoginEmail = function (address) {
         throw new Meteor.Error("Provided string is not an email")
     }
 
-    var user = Accounts.findUserByEmail(address)
+    var user = Accounts.findUserByEmail(address);
 
     if (!user) {
-        Accounts.createUser({email: address})
-        user = Accounts.findUserByEmail(address)
+        Accounts.createUser({email: address});
+        user = Accounts.findUserByEmail(address);
     }
 
-    var tokenRecord = Accounts._generateStampedLoginToken()
+    var tokenRecord = Accounts._generateStampedLoginToken();
     Accounts._insertLoginToken(user._id, tokenRecord);
 
-    var loginUrl = Meteor.absoluteUrl('login/' + tokenRecord.token)
-    console.info(loginUrl);
+    var loginUrl = Meteor.absoluteUrl('login/' + tokenRecord.token);
 
-    var html = ''
+    var html = '<!DOCTYPE>' +
+               '<html>' +
+               '<head>' +
+                   '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
+               '</head>' +
+               '<body>';
+
     if (user.profile) {
-        html += '<p>Hey ' + user.profile.name + ',</p>'
-        html += "<p>To login, just follow this link.</p>"
-    }
-    else {
-        html += '<p>Hey there,</p>'
-        html += "<p>To register, just follow this link. A user wil be created for you.</p>"
+        html += '<p>Tere ' + user.profile.name + ',</p>';
+        html += "<p>Toitla keskkonda sisselogimiseks kliki allolevale lingile: </p>";
+    } else {
+        html += '<p>Tere, hea huviline!</p>';
+        html += "<p>Tahad liituda Toitla kokkade tegusa kogukonnaga? Väga vahva! Algus on tehtud.</p>";
+        html += "<p>Toitla keskkonda sisselogimiseks kliki allolevale lingile: </p>";
     }
 
-    html += '<p><a target="_blank" href="' + loginUrl + '">' + loginUrl + '</a></p>'
+    html += '<p><a target="_blank" href="' + loginUrl + '">' + loginUrl + '</a></p>';
 
-    html += '<p>All the best, Toitla</p>'
+    if (!user.profile) {
+        html += '<p>Meie foorumist saad abi edasiste sammude tegemiseks. Foorumi lingi leiad Toitla lehe ülevalt servast või külasta <a target="_blank" href="spice.toitla.com">spice.toitla.com</a> (pead olema eelnevalt Toitla lehele sisse loginud).</p>';
+    }
+
+    html += '<p>Kohtumiseni!<br>Toitla kirjatuvi</p>';
+    html += '</body>' +
+            '</html>';
 
     Email.send({
         from: Settings.system_email,
         to: address,
-        subject: 'Toitla login link',
+        subject: 'Toitla ligipääs',
         html: html
     })
 };
