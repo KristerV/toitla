ac77d2f0a7c7c1d1745e62de0fc288031e6ff056
Trevor Himma <trevor@snowhound.eu>
2016-12-03 13:57:10 +0200

    Added payment block to order manager view

--- client/components/Order/OrderManager/OrderManagerInfo.jsx
+++ client/components/Order/OrderManager/OrderManagerInfo.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 OrderManagerInfo = React.createClass({
 
     render() {
-        var order = this.props.order || {}
-        order.errors = order.errors || {}
+        var order = this.props.order || {};
+        order.errors = order.errors || {};
         return(<div className="mdl-grid">
             <div className="mdl-cell mdl-cell--4-col">
                 <OrderContactForm order={order}/>
@@ -13,6 +13,9 @@ OrderManagerInfo = React.createClass({
             </div>
             <div className="mdl-cell mdl-cell--4-col">
                 <OrderAllergiesForm order={order}/>
+            </div>
+            <div className="mdl-cell mdl-cell--4-col">
+                <OrderPaymentForm order={order}/>
             </div>
             <div className="mdl-cell mdl-cell--8-col">
                 <OrderGeneralInputForm
@@ -25,4 +28,4 @@ OrderManagerInfo = React.createClass({
             </div>
         </div>)
     }
-})
+});
